98aa81576214be073d9bb37d0838171adf36263f
NenadStulic <49113707+NenadStulic@users.noreply.github.com>
2019-10-23 16:04:43 +0200

    Add files via upload

---
+++ finalProject/js/gallery.js
@@ -0,0 +1,6 @@
+
+[...document.querySelectorAll('.single-column')].map(column => {
+	column.style.setProperty('--animation', 'slide');
+	column.style.setProperty('height', '200%');
+	column.innerHTML = column.innerHTML + column.innerHTML;
+});
